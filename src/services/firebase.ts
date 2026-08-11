44902dd761a31773481ebc0e61d3d1ae2724f17e
Keshav Sharma <Keshavs.0496@gmail.com>
2026-08-11 21:22:59 +0530

    TEMP: updating for pop-up blocked error.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { browserPopupRedirectResolver, getAuth, initializeAuth } from "firebase/auth";
 import { 
   initializeFirestore, 
   getFirestore, 
@@ -9,25 +9,19 @@ import {
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY || "AIzaSyACo07xPXMMfG2XzEnPJMRJ2X62gCYIx94",
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || "trackr-72842.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "trackr-72842",
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET || "trackr-72842.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || "2562502585",
-  appId: process.env.NEXT_PUBLIC_APP_ID || "1:2562502585:web:fe715d4049733b4f279068",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyACo07xPXMMfG2XzEnPJMRJ2X62gCYIx94",
-//   authDomain: "trackr-72842.firebaseapp.com",
-//   projectId: "trackr-72842",
-//   storageBucket: "trackr-72842.firebasestorage.app",
-//   messagingSenderId: "2562502585",
-//   appId: "1:2562502585:web:fe715d4049733b4f279068"
-// };
 
 // Initialize Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  popupRedirectResolver: browserPopupRedirectResolver,
+});
 
 // Initialize Firestore with client-side offline persistence enabled
 const db = typeof window !== "undefined"
@@ -37,6 +31,14 @@ const db = typeof window !== "undefined"
       }),
     })
   : getFirestore(app);
+
+  if (typeof window !== "undefined") {
+  console.log({
+    origin: window.location.origin,
+    authDomain: firebaseConfig.authDomain,
+    projectId: firebaseConfig.projectId,
+  });
+}
 
 const storage = getStorage(app);
 
